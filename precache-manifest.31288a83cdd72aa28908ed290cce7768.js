b7a77fcfd8d10c8778d89912352dd5403053845b
boryar <robrayuk@gmail.com>
2020-09-23 17:36:13 +0100

    Updates

---
+++ precache-manifest.31288a83cdd72aa28908ed290cce7768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38eb11f448f139a2fed6683ee4045a4f",
+    "url": "/monsterrolodex/index.html"
+  },
+  {
+    "revision": "e408095414fcde0794b4",
+    "url": "/monsterrolodex/static/css/main.317e5b44.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsterrolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterrolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e408095414fcde0794b4",
+    "url": "/monsterrolodex/static/js/main.a5cb9397.chunk.js"
+  },
+  {
+    "revision": "708ec28286b2b29e9880",
+    "url": "/monsterrolodex/static/js/runtime-main.832dc63f.js"
+  }
+]);
